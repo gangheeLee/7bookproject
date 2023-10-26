6e8624a6173d8b5f088ed7cd38a8d8927318eda1
gangheeLee <kalpha262@naver.com>
2023-10-26 13:46:32 +0900

    test12

--- src/component/pages/mypage/MyPage.js
+++ src/component/pages/mypage/MyPage.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const UserInfoBox = styled.div`
   height: 100vh;
@@ -55,16 +57,29 @@ const UserInfo = styled.div`
   margin-right: 100px;
 `;
 
-const MypageButton = styled.div`
+const MypageButton = styled.button`
   width: 100px;
+  height: 40px;
   border: none;
+  font-size: 16px;
   color: white;
   background-color: #774836;
   text-align: center;
   border-radius: 10px;
 `;
 
+const StyledTableTd = styled.td`
+  border: none;
+
+  &:last-child {
+    width: 500px;
+  }
+`;
+
 export default function MyPage() {
+  const notify = () => {
+    toast("회원정보가 수정되었습니다.");
+  };
   return (
     <UserInfoBox>
       <UserInfoMenu>
@@ -97,57 +112,54 @@ export default function MyPage() {
       <UserInfoTitle>
         회원정보 수정
         <UserInfo>
-          <table style={{}}>
+          <table style={{ border: "none" }}>
             <tr>
-              <td>이름</td>
-              <td style={{ width: "400px" }}>홍길동</td>
+              <StyledTableTd>이름</StyledTableTd>
+              <StyledTableTd>홍길동</StyledTableTd>
             </tr>
             <tr>
-              <td>아이디</td>
-              <td>aaaa1234</td>
+              <StyledTableTd>아이디</StyledTableTd>
+              <StyledTableTd>aaaa1234</StyledTableTd>
             </tr>
             <tr>
-              <td>비밀번호</td>
-              <td>
+              <StyledTableTd>비밀번호</StyledTableTd>
+              <StyledTableTd>
                 <input type="password" style={{ width: "300px" }}></input>
-              </td>
+              </StyledTableTd>
             </tr>
             <tr>
-              <td>비밀번호 확인</td>
-              <td>
+              <StyledTableTd>비밀번호 확인</StyledTableTd>
+              <StyledTableTd>
                 <input type="password" style={{ width: "300px" }}></input>
-              </td>
+              </StyledTableTd>
             </tr>
             <tr>
-              <td>이메일</td>
-              <td>
+              <StyledTableTd>이메일</StyledTableTd>
+              <StyledTableTd>
                 <input type="email" style={{ width: "300px" }}></input>
-              </td>
+              </StyledTableTd>
             </tr>
             <tr>
-              <td>휴대전화</td>
-              <td>
+              <StyledTableTd>휴대전화</StyledTableTd>
+              <StyledTableTd>
                 <input type="text" style={{ width: "300px" }}></input>
-              </td>
+              </StyledTableTd>
             </tr>
             <tr>
-              <td>생년월일</td>
-              <td>1999.01.01</td>
+              <StyledTableTd>생년월일</StyledTableTd>
+              <StyledTableTd>1999.01.01</StyledTableTd>
             </tr>
             <tr>
-              <td colSpan={"2"}>
-                <MypageButton>
-                  <Link
-                    to="/"
-                    style={{
-                      textDecoration: "none",
-                      color: "white",
-                    }}
-                  >
-                    수정
-                  </Link>
-                </MypageButton>
-              </td>
+              <StyledTableTd colSpan={"2"}>
+                <MypageButton onClick={notify}>수정</MypageButton>
+                <ToastContainer
+                  position="bottom-center"
+                  limit={999}
+                  closeButton={false}
+                  autoClose={1000}
+                  hideProgressBar
+                />
+              </StyledTableTd>
             </tr>
           </table>
         </UserInfo>
